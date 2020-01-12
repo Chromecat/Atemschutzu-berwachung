cf72b0d10b0936761bd50c76083682b81348b0e5
Stefan <dewenter.stefan@gmail.com>
2020-01-12 19:47:11 +0100

    es läuft

--- Informationextraction.py
+++ Informationextraction.py
@@ -1,27 +1,31 @@
 import pickle
 import nltk
 import functions
+import re
 
+# POS model laden
 with open('Part_Of_Speech_Model/nltk_german_classifier_data.pickle', 'rb') as f:
     tagger = pickle.load(f)
 
+# Konversation aus externer Datei laden
 Funkkonversation = open("conversation.txt", "r")
 Funkkonversation = Funkkonversation.read()
 
+# NLTK operationen
 sentence = nltk.sent_tokenize(Funkkonversation)  # satz seperation
 sentence = [nltk.word_tokenize(sent) for sent in sentence]  # wort seperation
 sentence = [tagger.tag(word) for word in sentence]  # tagging
 
 # Tags durch Grammatik ersetzen
-functions.replacetagtype("für|von", "NE", "NE", "X-FÜR", sentence)
+functions.replacetagtype("für|von", "NE", "NE", "X-VON", sentence)
 functions.replacetagtwo("kommen", "\.", "X-KOMMEN", sentence)
-functions.replacetag("trupp|Atemschutzüberwachung", "TRUPP", sentence)
+functions.replacetag("trupp|Gruppenführer|Atemschutzüberwachung", "TRUPP", sentence)
 functions.replacetag("Hier", "X-HIER", sentence)
 functions.replacetag("Frage", "X-FRAGE", sentence)
 functions.replacetagtype("Druck", "NE", "CARD", "DRUCK", sentence)
 functions.replacetagtwo("Verstanden", "Ende", "X-ENDE", sentence)
 
-print(sentence[0][0][0])
+#print(sentence[0][0][0])
 
 grammar = """
             EINHEIT: {<TRUPP><NE>?}
@@ -30,16 +34,41 @@ grammar = """
             Antwort auf Anfrage: {<X-HIER><EINHEIT>}
             Frage: {<X-FRAGE><APPR>?<NN>?}
             Ende des Dialogs: {<X-ENDE><NN><\$.>}
-            Druckangabe: {<DRUCK><CARD>?<CARD>?<CARD>?<CARD>}
+            Druckangabe: {<DRUCK><CARD>?<CARD>?<CARD>?<CARD>?}
 
           """
 
-# funktion schreiben die die Antwort auf die Frage erkennt
-print()
+grammar_parser = nltk.RegexpParser(grammar)
 
-cp = nltk.RegexpParser(grammar)
+Einheit1 = ""
+Einheit2 = ""
+
+# Schleife über alle Sätze
 for x in range(len(sentence)):
-    result = cp.parse(sentence[x])
-    # result.draw()
-    test = list(result)
-    print(test)
+
+    # Text wieder zusammenfügen
+    Text = ''
+    for words in range(len(sentence[x])):
+        Text += str(sentence[x][words][0]) + " "
+
+    # Grammatik anwenden
+    sentence_grammar = grammar_parser.parse(sentence[x])
+
+    print()
+    if re.findall("EINHEIT", str(sentence_grammar)):
+        if re.findall("X-VON", str(sentence_grammar)):
+            try:
+                Einheit1 = ("Einheit1: " + str(sentence_grammar[0][0][0]) + " " + str(sentence_grammar[0][1][0]))
+            except IndexError:
+                Einheit1 = ("Einheit1: " + str(sentence_grammar[0][0][0]))
+            try:
+                Einheit2 = ("Einheit2: " + str(sentence_grammar[2][0][0]) + " " + str(sentence_grammar[2][1][0]))
+            except IndexError:
+                Einheit2 = ("Einheit2: " + str(sentence_grammar[2][0][0]))
+
+    if re.findall("DRUCK", str(sentence_grammar)):
+        Druck = ("Druck: " + str(re.findall("[0-9]+", str(sentence_grammar))))
+    else: Druck = ""
+
+    print(Einheit1 + " " + Einheit2 + " " + Druck + " " + Text)
+
